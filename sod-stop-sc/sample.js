64812131ab15565fdc4d2ab408b91a224d5b796d
Gavin Mogan <gavin@saucelabs.com>
2015-11-10 13:08:14 -0800

    its 0.1.3 now to force clean installs

--- sod-stop-sc/sample.js
+++ sod-stop-sc/sample.js
@@ -3,16 +3,17 @@ var fs = require('fs');
 console.log('Getting PID path', process.env.SAUCE_CONNECT_PID_PATH );
 var pid_file = process.env.SAUCE_CONNECT_PID_PATH;
 if (!pid_file) {
-  console.error("No pid file exists");
+  console.error('No pid file exists');
   process.exit(1);
 }
 
 if (!fs.existsSync(pid_file)) {
-  console.error("Pid file doesn't exist", pid_file);
+  console.error('Pid file does not exist', pid_file);
   process.exit(1);
 }
 
 var pid = fs.readFileSync(pid_file);
 
-console.log('Now killing SC');
+console.log('Now killing Sauce Connect - ', pid);
 process.kill(pid);
+console.log('Finished killing Sauce Connect');
