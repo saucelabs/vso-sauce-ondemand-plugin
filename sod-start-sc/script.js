e477cdd8659c8db8c92f2ccefeb426dbd1193945
Gavin Mogan <gavin@saucelabs.com>
2015-11-10 16:01:30 -0800

    all the working components and ready to be packaged

--- sod-start-sc/script.js
+++ sod-start-sc/script.js
@@ -28,13 +28,12 @@ if (files.length !== 1) {
 }
 var version = regex.exec(files[0])[1];
 
-var sc_path_bin = path.join(binaries_path, 'sc-' + version + '-' + platform + arch, 'bin', 'sc');
+var sc_path_bin = path.join(binaries_path, 'sc-' + version + '-' + platform + arch, 'bin', (process.platform === 'win32' ? 'sc.exe' : 'sc'));
 fs.chmodSync(sc_path_bin, '0755');
 
 console.log('Running sauce connect: ', sc_path_bin);
 
-process.env.SAUCE_CONNECT_PID_PATH = path.join((process.env.BUILD_STAGINGDIRECTORY || self_path), 'sc.pid');
-tl.setVariable('SAUCE_CONNECT_PID_PATH', process.env.SAUCE_CONNECT_PID_PATH);
+process.env.SAUCE_CONNECT_PID = path.join((process.env.BUILD_STAGINGDIRECTORY || self_path), 'sc.pid');
 console.log('Setting PID path', process.env.SAUCE_CONNECT_PID_PATH );
 
 var sc_bin = spawn(
@@ -49,15 +48,37 @@ var sc_bin = spawn(
     cwd: self_path
   }
 );
-sc_bin.stdout.on('data', function (data) {
-  if (/Sauce Connect is up, you may start your tests/.test(data)) {
+
+tl.setVariable('SAUCE_CONNECT_PID_PATH', process.env.SAUCE_CONNECT_PID_PATH);
+tl.setVariable('SAUCE_CONNECT_PID', sc_bin.pid);
+
+
+var lineEmitter = new (require('events').EventEmitter)();
+lineEmitter.on('stdout', function(line) {
+  if (/Sauce Connect is up, you may start your tests/.test(line)) {
     process.exit(0); // SUCCESS
   }
-  console.log(data.toString());
+  console.log(line.toString());
+});
+lineEmitter.on('stderr', function(line) {
+  console.error(line.toString());
 });
 
-sc_bin.stderr.on('data', function (data) {
-  console.log(data.toString());
+var dataHolders = {};
+['stdout', 'stderr'].forEach(function(channel) {
+  dataHolders[channel] = [];
+  sc_bin[channel].on('data', function (data) {
+    data.toString().split('').forEach(function(char) {
+      if (char === '\n' || char === '\r') {
+        if (dataHolders[channel].length !== 0) {
+          lineEmitter.emit(channel, dataHolders[channel].join(''));
+          dataHolders[channel] = [];
+        }
+        return;
+      }
+      dataHolders[channel].push(char);
+    });
+  });
 });
 
 sc_bin.on('close', function(code) {
--- sod-stop-sc/sample.js
+++ sod-stop-sc/sample.js
@@ -1,18 +1,9 @@
-var fs = require('fs');
-
-console.log('Getting PID path', process.env.SAUCE_CONNECT_PID_PATH );
-var pid_file = process.env.SAUCE_CONNECT_PID_PATH;
-if (!pid_file) {
-  console.error('No pid file exists');
+console.log('Getting SC PID: ', process.env.SAUCE_CONNECT_PID );
+var pid = process.env.SAUCE_CONNECT_PID.toString();
+if (!pid) {
+  console.error('No pid provided, so bailing');
   process.exit(1);
 }
-
-if (!fs.existsSync(pid_file)) {
-  console.error('Pid file does not exist', pid_file);
-  process.exit(1);
-}
-
-var pid = fs.readFileSync(pid_file);
 
 console.log('Now killing Sauce Connect - ', pid);
 process.kill(pid);
