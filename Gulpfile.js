028da5f81db5ab47b273f16702d2d8a32b2796b0
Gavin Mogan <gavin@saucelabs.com>
2015-12-09 11:33:43 -0800

    webpack producing all the right files

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,13 +3,27 @@ var del = require('del');
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
-gulp.tfx_extension_create = require('tfx-cli/_build/app/exec/extension/create');
-gulp.tfx_extension_publish = require('tfx-cli/_build/app/exec/extension/publish');
-gulp.jsonTransform = require('gulp-json-transform');
-gulp.copy = require('gulp-copy');
+var webpack = require('webpack-stream');
+var named = require('vinyl-named');
+
+var tfx_extension_create = require('tfx-cli/_build/app/exec/extension/create');
+var jsonTransform = require('gulp-json-transform');
+var copy = require('gulp-copy');
 
 var pkg = require('./package.json');
 
+var fileTasks = {
+  'sod-build-info:js': ['./sod-build-info/scripts/*.js'],
+  'copy': [
+    'images/**/*',
+    'overview.md',
+    'sod-main/**/*', 
+    'sod-stop-sc/**/*', 
+    'sod-build-info/**/*', 
+    '!./sod-build-info/scripts/*.js',
+    '!./sod-*/task.json'
+  ]
+};
 gulp.task('clean', function() {
   return del([
     './dist/*'
@@ -18,7 +32,7 @@ gulp.task('clean', function() {
 
 gulp.task('vss-extension', function() {
   return gulp.src('./vss-extension.json')
-    .pipe(gulp.jsonTransform(function(data) {
+    .pipe(jsonTransform(function(data) {
       data.version = pkg.version;
       if (process.env.NODE_ENV !== 'production') {
         data.publisher = 'saucelabs-beta';
@@ -30,8 +44,8 @@ gulp.task('vss-extension', function() {
 });
 
 gulp.task('copy', function() {
-  return gulp.src(['./images/**/*', 'overview.md', 'sod-main/**/*', 'sod-stop-sc/**/*'])
-    .pipe(gulp.copy('./dist'));
+  return gulp.src(fileTasks.copy)
+    .pipe(copy('./dist'));
 });
 
 var fix_task_version = function(data) {
@@ -44,20 +58,47 @@ var fix_task_version = function(data) {
 
 gulp.task('sod-main-task', ['copy'], function() {
   return gulp.src('./sod-main/task.json')
-    .pipe(gulp.jsonTransform(fix_task_version))
+    .pipe(jsonTransform(fix_task_version))
     .pipe(gulp.dest('./dist/sod-main/'));
 });
 
 gulp.task('sod-stop-sc', ['copy'], function() {
   return gulp.src('./sod-main/task.json')
-    .pipe(gulp.jsonTransform(fix_task_version))
+    .pipe(jsonTransform(fix_task_version))
     .pipe(gulp.dest('./dist/sod-main/'));
 });
 
+gulp.task('sod-build-info:js', function() {
+  return gulp.src(fileTasks['sod-build-info:js'])
+    .pipe(named())
+    .pipe(webpack({
+      output: {
+        libraryTarget: 'amd'
+      },
+      resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+      },
+      module: {
+        loaders: [
+          { 
+            test: /\.js$/, 
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: {
+              presets: ['es2015']
+            }
+          },
+          { test: /\.css$/, loader: 'style!css' }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('dist/sod-build-info/scripts'));
+});
+
 gulp.task('package', function(cb) {
-  runSequence('clean', 'default', function() {
+  //runSequence('clean', 'default', function() {
     var common = require('tfx-cli/_build/app/lib/common');
-    var command = gulp.tfx_extension_create.getCommand([
+    var command = tfx_extension_create.getCommand([
       '--output-path', path.join(__dirname, 'Packages'),
       '--root', path.join(__dirname, 'dist')
     ]);
@@ -66,9 +107,15 @@ gulp.task('package', function(cb) {
       console.log('then', arguments);
       cb();
     }, function(reason) {
-      console.error('Unable to upload because ', reason);
+      console.error('Unable to create package because ', reason);
       cb(reason);
     });
+  //});
+});
+
+gulp.task('watch', function() {
+  Object.keys(fileTasks).forEach(function(key) {
+    gulp.watch(fileTasks[key], [key]);
   });
 });
 
@@ -77,7 +124,8 @@ gulp.task('default', [
   'vss-extension',
   'copy',
   'sod-main-task',
-  'sod-stop-sc'
+  'sod-stop-sc',
+  'sod-build-info:js'
 ], function(){
   // run tasks here
   // set up watch handlers here
